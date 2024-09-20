a26cad05df58c1fb35a8301c2465ef1dd3fc3a09
joaomoraiss <joaovictorrmpework@outlook.com>
2024-09-20 10:17:16 -0300

    Departament class

---
+++ src/secao12/a123/Departament.java
@@ -0,0 +1,17 @@
+package secao12.a123;
+
+public class Departament {
+    private String name;
+
+    public Departament(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
