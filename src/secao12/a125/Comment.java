53a1f0cab6fed6d948ce6a261766d3187c841354
joaomoraiss <joaovictorrmpework@outlook.com>
2024-09-23 17:45:11 -0300

    comment class

---
+++ src/secao12/a125/Comment.java
@@ -0,0 +1,17 @@
+package secao12.a125;
+
+public class Comment {
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public Comment(String text) {
+        this.text = text;
+    }
+
+    private String text;
+}
