0ba64643d63dbe62e48bc1cdb5ca6b1daf13ed0f
joaomoraiss <joaovictorrmpework@outlook.com>
2024-11-21 12:00:30 -0300

    ex135

---
+++ src/secao13/a135/ex135.java
@@ -0,0 +1,19 @@
+package secao13.a135;
+
+import java.util.Scanner;
+
+public class ex135 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Quantos funcionarios?");
+        int fqtd = sc.nextInt();
+        for (int i = 0; i < fqtd; i++){
+            System.out.println("Nome:");
+            String nome = sc.nextLine();
+            System.out.println("Horas:");
+            int horas = sc.nextInt();
+            System.out.println("valor por hora:");
+            Double valorpHora = sc.nextDouble();
+            Employee employee = new Employee(nome, horas, valorpHora);
+        }
+}
